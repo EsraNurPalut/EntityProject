fad9cbec7ff191032f34a6727c751f17a9cb3355
EsraNurPalut <nuresrapalut@outlook.com>
2022-10-20 22:05:05 +0300

    Form1 güncellendi.

--- EntityCodeFirst/Form1.Designer.cs
+++ EntityCodeFirst/Form1.Designer.cs
@@ -29,13 +29,47 @@ namespace EntityCodeFirst
         /// </summary>
         private void InitializeComponent()
         {
+            this.button1 = new System.Windows.Forms.Button();
+            this.button2 = new System.Windows.Forms.Button();
+            this.button3 = new System.Windows.Forms.Button();
             this.SuspendLayout();
+            // 
+            // button1
+            // 
+            this.button1.Location = new System.Drawing.Point(115, 64);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(100, 138);
+            this.button1.TabIndex = 0;
+            this.button1.Text = "MÜŞTERİLER";
+            this.button1.UseVisualStyleBackColor = true;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // button2
+            // 
+            this.button2.Location = new System.Drawing.Point(325, 64);
+            this.button2.Name = "button2";
+            this.button2.Size = new System.Drawing.Size(100, 138);
+            this.button2.TabIndex = 1;
+            this.button2.Text = "ÜRÜNLER";
+            this.button2.UseVisualStyleBackColor = true;
+            // 
+            // button3
+            // 
+            this.button3.Location = new System.Drawing.Point(552, 64);
+            this.button3.Name = "button3";
+            this.button3.Size = new System.Drawing.Size(100, 138);
+            this.button3.TabIndex = 2;
+            this.button3.Text = "KATEGORİLER";
+            this.button3.UseVisualStyleBackColor = true;
             // 
             // Form1
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
             this.ClientSize = new System.Drawing.Size(800, 450);
+            this.Controls.Add(this.button3);
+            this.Controls.Add(this.button2);
+            this.Controls.Add(this.button1);
             this.Name = "Form1";
             this.Text = "Form1";
             this.Load += new System.EventHandler(this.Form1_Load);
@@ -44,6 +78,10 @@ namespace EntityCodeFirst
         }
 
         #endregion
+
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.Button button2;
+        private System.Windows.Forms.Button button3;
     }
 }
 
--- EntityCodeFirst/Form1.cs
+++ EntityCodeFirst/Form1.cs
@@ -22,5 +22,11 @@ namespace EntityCodeFirst
             Context c = new Context();
             c.Database.Create();
         }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+         
+
+        }
     }
 }
