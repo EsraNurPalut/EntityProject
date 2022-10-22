c7d5642dcef63ec387d9a35dc1e624f10fd991b2
EsraNurPalut <nuresrapalut@outlook.com>
2022-10-22 22:12:14 +0300

    raporlama düzenlendi.

---
+++ EntityCodeFirst/Entity/Raporlama.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace EntityCodeFirst.Entity
+{
+  public   class Raporlama
+    {
+        [Key]
+        public int raporID { get; set; }
+        public string raporad { get; set; }
+        public string rapormarka { get; set; }
+    }
+}
